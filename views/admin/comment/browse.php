386dcb5294c98ba8e51810afe8ccdec27a64ead2
Patrick Murray-John <patrickmjchnm@gmail.com>
2011-12-22 14:46:47 -0500

    Beginning redo

--- views/admin/comment/browse.php
+++ views/admin/comment/browse.php
@@ -22,31 +22,7 @@ queue_js('commenting');
         </div>
     </div>
 <h1>Comments</h1>
-<table>
-<thead>
-<tr>
-<th>Record</th>
-<th>Author</th>
-<th>Url</th>
-<th>Email</th>
-<th>Comment</th>
-<th>Approved</th>
-</tr>
-</thead>
-<tbody>
-<?php foreach($this->comments as $comment): ?>
-<tr>
-<td><a href="<?php echo commenting_comment_uri($comment); ?>">Link</a></td>
-<td><?php echo $comment->author_name; ?></td>
-<td><?php echo $comment->author_url; ?></td>
-<td><?php echo $comment->author_email; ?></td>
-<td><?php echo $comment->body; ?></td>
-<td><?php echo $comment->approved ? 'Yes' : "<span class='approve' id='approve-{$comment->id}'>Approve</span>"; ?></td>
-</tr>
-<?php endforeach; ?>
-</tbody>
-
-</table>
+<?php echo commenting_render_comments($comments); ?>
 </div>
 
 <?php foot(); ?>
