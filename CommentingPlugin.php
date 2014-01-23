847ed88af88a76c83b290c5fd801e40759fd4e17
patrickmj <patrickmjchnm@gmail.com>
2014-01-23 15:10:45 -0500

    make upgrade get correct table name

--- CommentingPlugin.php
+++ CommentingPlugin.php
@@ -7,7 +7,7 @@ class CommentingPlugin extends Omeka_Plugin_AbstractPlugin
     protected $_hooks = array(
         'install',
         'uninstall',
-        'public_items_show',        
+        'public_items_show',
         'public_collections_show',
         'public_head',
         'admin_head',
@@ -24,26 +24,26 @@ class CommentingPlugin extends Omeka_Plugin_AbstractPlugin
         'search_record_types',
         'api_resources',
         'api_extend_items',
-        'api_extend_collections'          
+        'api_extend_collections'
     );
-    
+
     /**
      * Add the translations.
      */
     public function hookInitialize()
     {
         add_translation_source(dirname(__FILE__) . '/languages');
-    }    
+    }
 
     public function setUp()
     {
-        
+
         if(plugin_is_active('SimplePages')) {
             $this->_filters[] = 'api_extend_simple_pages';
         }
         parent::setUp();
     }
-    
+
     public function hookInstall()
     {
         $db = $this->_db;
@@ -88,31 +88,31 @@ class CommentingPlugin extends Omeka_Plugin_AbstractPlugin
                 $commentRoles = array('super');
                 set_option('commenting_comment_roles', serialize($commentRoles));
             }
-            
+
             if(!get_option('commenting_moderate_roles')) {
                 $moderateRoles = array('super');
                 set_option('commenting_moderate_roles', serialize($moderateRoles));
             }
-            
+
             if(!get_option('commenting_noapp_comment_roles')) {
                 set_option('commenting_noapp_comment_roles', serialize(array()));
             }
-            
+
             if(!get_option('commenting_view_roles')) {
                 set_option('commenting_view_roles', serialize(array()));
-            }            
+            }
         }
-        
+
         if(version_compare($old, '2.1-rc', '<')) {
             delete_option('commenting_noapp_comment_roles');
             set_option('commenting_reqapp_comment_roles', serialize(array()));
             if($old == '1.1') {
                 $sql = "ALTER TABLE `$db->Comment` ADD `flagged` BOOLEAN NOT NULL DEFAULT '0' AFTER `approved` ";
             } else {
-                $sql = "ALTER TABLE `omeka_comments` CHANGE `flagged` `flagged` TINYINT( 1 ) NOT NULL DEFAULT '0'";
+                $sql = "ALTER TABLE `$db->Comment` CHANGE `flagged` `flagged` TINYINT( 1 ) NOT NULL DEFAULT '0'";
             }
-            
-            $db->query($sql);            
+
+            $db->query($sql);
         }
     }
 
@@ -135,7 +135,7 @@ class CommentingPlugin extends Omeka_Plugin_AbstractPlugin
     {
         queue_css_file('commenting');
     }
-    
+
     public function hookAfterDeleteRecord($args)
     {
         $record = $args['record'];
@@ -147,33 +147,33 @@ class CommentingPlugin extends Omeka_Plugin_AbstractPlugin
     }
 
     public static function showComments($args = array())
-    {    
+    {
         echo "<div id='comments-container'>";
-        if( (get_option('commenting_allow_public') == 1) 
-                || (get_option('commenting_allow_public_view') == 1) 
+        if( (get_option('commenting_allow_public') == 1)
+                || (get_option('commenting_allow_public_view') == 1)
                 || is_allowed('Commenting_Comment', 'show') ) {
             if(isset($args['view'])) {
                 $view = $args['view'];
             } else {
                 $view = get_view();
             }
-            
+
             $view->addHelperPath(COMMENTING_PLUGIN_DIR . '/helpers', 'Commenting_View_Helper_');
             $options = array('threaded'=> get_option('commenting_threaded'), 'approved'=>true);
-            
+
             $comments = isset($args['comments']) ? $args['comments'] : $view->getComments($options);
             echo $view->partial('comments.php', array('comments'=>$comments, 'threaded'=>$options['threaded']));
         }
-        
-        if( (get_option('commenting_allow_public') == 1) 
+
+        if( (get_option('commenting_allow_public') == 1)
                 || is_allowed('Commenting_Comment', 'add') ) {
             echo "<div id='comment-main-container'>";
             echo $view->getCommentForm();
             echo "</div>";
-        }    
+        }
         echo "</div>";
     }
-    
+
     public function hookPublicItemsShow($args)
     {
         $this::showComments($args);
@@ -208,7 +208,7 @@ class CommentingPlugin extends Omeka_Plugin_AbstractPlugin
     {
         $acl = $args['acl'];
         $acl->addResource('Commenting_Comment');
-        $commentRoles = unserialize(get_option('commenting_comment_roles'));        
+        $commentRoles = unserialize(get_option('commenting_comment_roles'));
         $moderateRoles = unserialize(get_option('commenting_moderate_roles'));
         $viewRoles = unserialize(get_option('commenting_view_roles'));
         $acl->allow(null, 'Commenting_Comment', array('flag'));
@@ -253,13 +253,13 @@ class CommentingPlugin extends Omeka_Plugin_AbstractPlugin
 
         return $tabs;
     }
-    
+
     public function filterSearchRecordTypes($types)
     {
         $types['Comment'] = __('Comments');
         return $types;
     }
-    
+
     public function filterApiResources($apiResources)
     {
         $apiResources['comments'] = array(
@@ -267,14 +267,14 @@ class CommentingPlugin extends Omeka_Plugin_AbstractPlugin
                 'actions' => array('get', 'index'),
                 'index_params' => array('record_type', 'record_id')
         );
-        return $apiResources;        
+        return $apiResources;
     }
-    
+
     public function filterApiExtendItems($extend, $args)
     {
         return $this->_filterApiExtendRecords($extend, $args);
     }
-    
+
     public function filterApiExtendCollections($extend, $args)
     {
         return $this->_filterApiExtendRecords($extend, $args);
@@ -283,8 +283,8 @@ class CommentingPlugin extends Omeka_Plugin_AbstractPlugin
     public function filterApiExtendSimplePages($extend, $args)
     {
         return $this->_filterApiExtendRecords($extend, $args);
-    }    
-    
+    }
+
     private function _filterApiExtendRecords($extend, $args)
     {
         $record = $args['record'];
@@ -294,10 +294,10 @@ class CommentingPlugin extends Omeka_Plugin_AbstractPlugin
                 'resource' => 'comments',
                 'url' => Omeka_Record_Api_AbstractRecordAdapter::getResourceUrl("/comments?record_type=$recordClass&record_id={$record->id}"),
                 );
-        
-        return $extend;        
+
+        return $extend;
     }
-    
+
     private function _countComments($record)
     {
         $params = array(
