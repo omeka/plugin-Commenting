0bda6e1c9a1c1acf1d547e6d43092ff28614dafe
patrickmj <patrickmjchnm@gmail.com>
2014-01-24 11:44:40 -0500

    fix 'flag inappropriate' button appearance

--- views/public/comment.php
+++ views/public/comment.php
@@ -1,5 +1,5 @@
 <div class='comment-author'>
-    <?php 
+    <?php
         if(!empty($comment->author_name)) {
             if(empty($comment->author_url)) {
                 $authorText = $comment->author_name;
@@ -8,10 +8,10 @@
             }
         } else {
             $authorText = __("Anonymous");
-        }   
+        }
     ?>
     <p class='comment-author-name'><?php echo $authorText?></p>
-    <?php 
+    <?php
         $hash = md5(strtolower(trim($comment->author_email)));
         $url = "http://www.gravatar.com/avatar/$hash";
         echo "<img class='gravatar' src='$url' />";
@@ -20,6 +20,6 @@
 <div class='comment-body <?php if($comment->flagged):?>comment-flagged<?php endif;?> '><?php echo $comment->body; ?></div>
 <?php if(is_allowed('Commenting_Comment', 'unflag')): ?>
 <p class='comment-flag' <?php if($comment->flagged): ?> style='display:none;'<?php endif;?> ><?php echo __("Flag inappropriate"); ?></p>
-<p class='comment-unflag' <?php if(!$comment->flagged): ?>style='display:none;'<?php endif;?> ><?php __("Unflag inappropriate"); ?></p>
+<p class='comment-unflag' <?php if(!$comment->flagged): ?>style='display:none;'<?php endif;?> ><?php echo __("Unflag inappropriate"); ?></p>
 <?php endif; ?>
 <p class='comment-reply'><?php echo __("Reply"); ?></p>
