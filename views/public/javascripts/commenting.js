732400c03086fc2b4795f0c804532efdb43a90f4
patrickmj <patrickmjchnm@gmail.com>
2018-01-22 15:49:41 -0500

    whitespace cleanup

--- views/public/javascripts/commenting.js
+++ views/public/javascripts/commenting.js
@@ -1,52 +1,52 @@
 var Commenting = {
-        
-    handleReply: function(event) {        
-        Commenting.moveForm(event);    
+
+    handleReply: function(event) {
+        Commenting.moveForm(event);
     },
-    
+
     finalizeMove: function() {
         jQuery('#comment-form-body_parent').attr('style', '')  
     },
-    
-	moveForm: function(event) {
-	    //first make tinyMCE go away so it is safe to move around in the DOM
-	    tinyMCE.execCommand('mceRemoveControl', false, 'comment-form-body');
-		jQuery('#comment-form').insertAfter(event.target);
-		commentId = Commenting.getCommentId(event.target);
-		jQuery('#parent-id').val(commentId);
-		tinyMCE.execCommand('mceAddControl', false, 'comment-form-body');
-	},
-	
-	flag: function(event) {
-	    var commentId = Commenting.getCommentId(event.target);
-	    var json = {'id': commentId }; 
-	    jQuery.post(Commenting.pluginRoot + "flag", json, Commenting.flagResponseHandler);
-	},
+
+    moveForm: function(event) {
+        //first make tinyMCE go away so it is safe to move around in the DOM
+        tinyMCE.execCommand('mceRemoveControl', false, 'comment-form-body');
+        jQuery('#comment-form').insertAfter(event.target);
+        commentId = Commenting.getCommentId(event.target);
+        jQuery('#parent-id').val(commentId);
+        tinyMCE.execCommand('mceAddControl', false, 'comment-form-body');
+    },
+
+    flag: function(event) {
+        var commentId = Commenting.getCommentId(event.target);
+        var json = {'id': commentId }; 
+        jQuery.post(Commenting.pluginRoot + "flag", json, Commenting.flagResponseHandler);
+    },
 
     unflag: function(event) {
         var commentId = Commenting.getCommentId(event.target);
         var json = {'id': commentId }; 
         jQuery.post(Commenting.pluginRoot + "unflag", json, Commenting.flagResponseHandler);
-    },	
-	
-	flagResponseHandler: function(response, status, jqxhr) {
-	    var comment = jQuery('#comment-' + response.id);
-	    if(response.action == 'flagged') {
-	        comment.find('div.comment-body').addClass('comment-flagged');
-	        comment.find('p.comment-flag').hide();
-	        comment.find('p.comment-unflag').show();
-	    }
-	    
-	    if(response.action == 'unflagged') {
-	        comment.find('div.comment-body').removeClass('comment-flagged');
-	        comment.find('p.comment-flag').show();	        
-	        comment.find('p.comment-unflag').hide();
-	    }
-	},
-	
-	getCommentId: function(el) {
-	    return jQuery(el).parents('div.comment').first().attr('id').substring(8);
-	}
+    },
+
+    flagResponseHandler: function(response, status, jqxhr) {
+        var comment = jQuery('#comment-' + response.id);
+        if(response.action == 'flagged') {
+            comment.find('div.comment-body').addClass('comment-flagged');
+            comment.find('p.comment-flag').hide();
+            comment.find('p.comment-unflag').show();
+        }
+
+        if(response.action == 'unflagged') {
+            comment.find('div.comment-body').removeClass('comment-flagged');
+            comment.find('p.comment-flag').show();            
+            comment.find('p.comment-unflag').hide();
+        }
+    },
+
+    getCommentId: function(el) {
+        return jQuery(el).parents('div.comment').first().attr('id').substring(8);
+    }
 };
 
 /**
@@ -79,7 +79,7 @@ if(typeof Omeka.wysiwyg == 'undefined') {
                 media_strict: false,
                 width: "100%"
             };
-    
+
         // Overwrite default params with user-passed ones.
         for (var attribute in params) {
             // Account for annoying scripts that mess with prototypes.
@@ -89,21 +89,17 @@ if(typeof Omeka.wysiwyg == 'undefined') {
         }
         tinyMCE.init(initParams);
     };
-
 }
 
-jQuery(document).ready(function() {	
-	jQuery('.comment-reply').click(Commenting.handleReply);
-	jQuery('.comment-flag').click(Commenting.flag);
-	jQuery('.comment-unflag').click(Commenting.unflag);
-	var commentingWysiwyg = {
-	        elements: 'comment-form-body',
-	        mode: 'exact',
-	        valid_child_elements: "ul[li],ol[li]",
-	        theme_advanced_buttons1: "bold,italic,underline,link,bullist,numlist,|,code",
-	    };	        
-	Omeka.wysiwyg(commentingWysiwyg);
+jQuery(document).ready(function() {    
+    jQuery('.comment-reply').click(Commenting.handleReply);
+    jQuery('.comment-flag').click(Commenting.flag);
+    jQuery('.comment-unflag').click(Commenting.unflag);
+    var commentingWysiwyg = {
+            elements: 'comment-form-body',
+            mode: 'exact',
+            valid_child_elements: "ul[li],ol[li]",
+            theme_advanced_buttons1: "bold,italic,underline,link,bullist,numlist,|,code",
+        };
+    Omeka.wysiwyg(commentingWysiwyg);
 });
-		
-		
-	
